fbfaf3daa03659b2c8bca6054b44e1e92ebf74d2
zhenshen-mla <60142685+zhenshen-mla@users.noreply.github.com>
2020-08-01 19:32:10 +0800

    upload files

---
+++ utils/lr_scheduler.py
@@ -0,0 +1,48 @@
+import math
+
+
+class LR_Scheduler(object):
+
+    def __init__(self, mode, base_lr, num_epochs, iters_per_epoch=0,
+                 lr_step=0, warmup_epochs=0):
+        self.mode = mode
+        print('Using {} LR Scheduler!'.format(self.mode))
+        self.lr = base_lr
+        if mode == 'step':
+            assert lr_step
+        self.lr_step = lr_step
+        self.iters_per_epoch = iters_per_epoch
+        self.N = num_epochs * iters_per_epoch
+        self.epoch = -1
+        self.warmup_iters = warmup_epochs * iters_per_epoch
+        self.num_epochs = num_epochs
+
+    def __call__(self, optimizer, i, epoch, best_pred1, best_pred2):
+        T = epoch * self.iters_per_epoch + i
+        if self.mode == 'cos':
+            lr = 0.5 * self.lr * (1 + math.cos(1.0 * T / self.N * math.pi))
+        elif self.mode == 'poly':
+            lr = self.lr * pow((1 - 1.0 * T / self.N), 0.9)
+        elif self.mode == 'step':
+            lr = self.lr * (0.1 ** (epoch // self.lr_step))
+        else:
+            raise NotImplemented
+
+        # warm up lr schedule
+        if self.warmup_iters > 0 and T < self.warmup_iters:
+            lr = lr * 1.0 * T / self.warmup_iters
+        if epoch > self.epoch:
+            print('\n=>Epoches %i, learning rate = %.4f, \
+                previous best1 = %.4f, previous best2 = %.4f' % (epoch, lr, best_pred1, best_pred2))
+            self.epoch = epoch
+        assert lr >= 0
+        self._adjust_learning_rate(optimizer, lr)
+
+    def _adjust_learning_rate(self, optimizer, lr):
+        if len(optimizer.param_groups) == 1:
+            optimizer.param_groups[0]['lr'] = lr
+        else:
+            # enlarge the lr at the head
+            optimizer.param_groups[0]['lr'] = lr
+            for i in range(1, len(optimizer.param_groups)):
+                optimizer.param_groups[i]['lr'] = lr * 10
